8fd6a65dfcd8fb17f0c8ae0211e8900d444b65d0
Nicholas Cristofaro <nicholaspcr@gmail.com>
2022-07-30 18:02:29 -0300

    models: Update populations

--- pkg/models/element.go
+++ pkg/models/element.go
@@ -1,33 +1,62 @@
 package models
 
-// Vector -> Element of population
+// Vector -> Element of population.
 type Vector struct {
 	X      []float64 `json:"x"      yaml:"x"`
 	Objs   []float64 `json:"objs"   yaml:"objs"`
 	Crwdst float64   `json:"crwdst" yaml:"crwdst"`
 }
 
-// Copy the entire struct
-func (e *Vector) Copy() Vector {
+// Copy the entire struct.
+func (v *Vector) Copy() Vector {
 	var ret = Vector{}
 
-	ret.X = make([]float64, len(e.X))
-	copy(ret.X, e.X)
+	ret.X = make([]float64, len(v.X))
+	copy(ret.X, v.X)
 
-	ret.Objs = make([]float64, len(e.Objs))
-	copy(ret.Objs, e.Objs)
+	ret.Objs = make([]float64, len(v.Objs))
+	copy(ret.Objs, v.Objs)
 
 	return ret
 }
 
-// Population is a slice of the type Vector
-type Population []Vector
+// Population is a slice of the type Vector.
+type Population struct {
+	Vectors        []Vector `json:"vectors" yaml:"vectors"`
+	DimensionsSize int      `json:"dim_size" yaml:"dim_size"`
+	ObjectivesSize int      `json:"obj_size" yaml:"obj_size"`
+}
 
-// Copy of the []Vector slice
-func (e Population) Copy() Population {
-	arr := make(Population, len(e))
-	for i, v := range e {
-		arr[i] = v.Copy()
+// Size of the current Vector slice.
+func (p *Population) Size() int {
+	if p == nil {
+		return 0
 	}
-	return arr
+	return len(p.Vectors)
+}
+
+func (p *Population) DimSize() int {
+	if p == nil {
+		return 0
+	}
+	return p.ObjectivesSize
+}
+
+// ObjsSize of the current Vector slice.
+func (p *Population) ObjSize() int {
+	if p == nil {
+		return 0
+	}
+	return p.DimensionsSize
+}
+
+// Copy of the Vector slice.
+func (p *Population) Copy() Population {
+	population := &Population{
+		Vectors:        make([]Vector, p.Size()),
+		DimensionsSize: p.DimSize(),
+		ObjectivesSize: p.ObjSize(),
+	}
+	copy(population.Vectors, p.Vectors)
+	return *population
 }

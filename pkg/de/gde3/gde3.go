5da263a57dfa5d00cd604bdc7a26524ad206ada5
Nicholas Cristofaro <nicholaspcr@gmail.com>
2022-08-02 22:06:48 -0300

    Update gde3 implementation with methods of the Algorithm interface

--- pkg/de/gde3/gde3.go
+++ pkg/de/gde3/gde3.go
@@ -1,10 +1,9 @@
 package gde3
 
 import (
+	"context"
 	"math/rand"
-	"os"
 
-	"github.com/nicholaspcr/GoDE/internal/writer"
 	"github.com/nicholaspcr/GoDE/pkg/de"
 	"github.com/nicholaspcr/GoDE/pkg/models"
 	"github.com/nicholaspcr/GoDE/pkg/problems"
@@ -16,45 +15,31 @@ type gde3 struct{}
 
 // GDE3 Returns an instance of an object that implements the GDE3 algorithm. It
 // is compliant with the Mode
-func GDE3() de.Mode {
+func New() de.Algorithm {
 	return &gde3{}
-}
-
-// TODO: Remove
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
 
 // Execute is responsible for receiving the standard parameters defined in the
 // Mode and executing the gde3 algorithm
 func (g *gde3) Execute(
-	rankedCh chan<- models.Population,
-	maximumObjs chan<- []float64,
-	p de.AlgorithmParams,
+	ctx context.Context,
+	population models.Population,
 	problem problems.Interface,
 	variant variants.Interface,
-	population models.Population,
-	f *os.File,
-) {
-	defer func() {
-		_ = f.Close()
-	}()
+	store de.Store,
+	pareto chan<- []models.Vector,
 
-	// var writer *csv.Writer
-	w := writer.NewWriter(f)
-	w.Comma = ';'
-
+) error {
+	GEN := de.FetchGenerations(ctx)
+	dimSize := population.ObjSize()
 	// maximum objs found
-	maxObjs := make([]float64, p.M)
-
+	maxObjs := make([]float64, dimSize)
 	// calculates the objs of the inital population
-	for i := range population {
-		err := problem.Evaluate(&population[i], p.M)
-		checkError(err)
-
-		for j, obj := range population[i].Objs {
+	for i := range population.Vectors {
+		if err := problem.Evaluate(&population.Vectors[i], dimSize); err != nil {
+			return err
+		}
+		for j, obj := range population.Vectors[i].Objs {
 			if obj > maxObjs[j] {
 				maxObjs[j] = obj
 			}
@@ -62,92 +47,96 @@ func (g *gde3) Execute(
 	}
 
 	// writes the header in this execution's file
-	if err := w.WriteHeader(p.M); err != nil {
-		panic(err)
+	if err := store.Header(); err != nil {
+		// TODO: Add header contents to methods
+		return err
 	}
 	// writes the inital generation
-	if err := w.ElementsObjs(population); err != nil {
+	if err := store.Population(population); err != nil {
 		panic(err)
 	}
 
 	// stores the rank[0] of each generation
-	bestElems := make(models.Population, 0)
+	bestElems := make([]models.Vector, population.DimSize())
 
-	var genRankZero models.Population
-	var bestInGen models.Population
+	var genRankZero []models.Vector
+	var bestInGen []models.Vector
 	var trial models.Vector
 
-	for g := 0; g < p.GEN; g++ {
+	for g := 0; g < GEN; g++ {
 		// gets non dominated of the current population
-		genRankZero, _ = de.FilterDominated(population)
+		genRankZero, _ = de.FilterDominated(population.Vectors)
 
-		for i := 0; i < len(population); i++ {
-
+		for i := 0; i < len(population.Vectors); i++ {
 			// generates the mutatated vector
 			vr, err := variant.Mutate(
-				population,
-				genRankZero,
+				population.Vectors,
+					genRankZero,
 				variants.Parameters{
-					DIM:     p.DIM,
-					F:       p.F,
+					DIM:     population.DimSize(),
+					F:       de.FetchFConst(ctx),
 					CurrPos: i,
-					P:       p.P,
+					P:       de.FetchPConst(ctx),
 				})
-			checkError(err)
+			if err != nil {
+				return err
+			}
 
 			// trial element
-			trial = population[i].Copy()
+			trial = population.Vectors[i].Copy()
 
 			// CROSS OVER
-			currInd := rand.Int() % p.DIM
-			luckyIndex := rand.Int() % p.DIM
+			currInd := rand.Int() % population.DimSize()
+			luckyIndex := rand.Int() % population.DimSize()
 
-			for j := 0; j < p.DIM; j++ {
+			for j := 0; j < population.DimSize(); j++ {
 				changeProb := rand.Float64()
-				if changeProb < p.CR || currInd == luckyIndex {
+				if changeProb < de.FetchCRConst(ctx) || currInd == luckyIndex {
 					trial.X[currInd] = vr.X[currInd]
 				}
 
-				if trial.X[currInd] < p.FLOOR[currInd] {
-					trial.X[currInd] = p.FLOOR[currInd]
+				if trial.X[currInd] < population.Floors()[currInd] {
+					trial.X[currInd] = population.Floors()[currInd]
 				}
-				if trial.X[currInd] > p.CEIL[currInd] {
-					trial.X[currInd] = p.CEIL[currInd]
+				if trial.X[currInd] > population.Ceils()[currInd] {
+					trial.X[currInd] = population.Ceils()[currInd]
 				}
-				currInd = (currInd + 1) % p.DIM
+				currInd = (currInd + 1) % population.DimSize()
 			}
 
-			evalErr := problem.Evaluate(&trial, p.M)
-			checkError(evalErr)
+			if err := problem.Evaluate(&trial, dimSize); err != nil {
+				return err
+			}
 
 			// SELECTION
-			comp := de.DominanceTest(population[i].Objs, trial.Objs)
+			comp := de.DominanceTest(population.Vectors[i].Objs, trial.Objs)
 			if comp == 1 {
-				population[i] = trial.Copy()
-			} else if comp == 0 && len(population) <= 2*p.NP {
-				population = append(population, trial.Copy())
+				population.Vectors[i] = trial.Copy()
+			} else if comp == 0 && len(population.Vectors) <= 2*population.Size() {
+				population.Vectors = append(population.Vectors, trial.Copy())
 			}
 		}
 
-		population, bestInGen = de.ReduceByCrowdDistance(population, p.NP)
+		population.Vectors, bestInGen = de.ReduceByCrowdDistance(population.Vectors, population.Size())
 		bestElems = append(bestElems, bestInGen...)
 
 		// writes the objectives of the population
-		if err := w.ElementsObjs(population); err != nil {
-			panic(err)
+		if err := store.Population(population); err != nil {
+			return err
 		}
 
-		// checks for the biggest objective
-		for _, p := range population {
-			for j, obj := range p.Objs {
-				if obj > maxObjs[j] {
-					maxObjs[j] = obj
-				}
-			}
-		}
+		//// checks for the biggest objective
+		//for _, vector := range population.Vectors {
+		//	for j, obj := range vector.Objs {
+		//		if obj > maxObjs[j] {
+		//			maxObjs[j] = obj
+		//		}
+		//	}
+		//}
 	}
 
 	// sending via channel the data
-	rankedCh <- bestElems
-	maximumObjs <- maxObjs
+	// maximumObjs <- maxObjs
+	pareto <- bestElems
+	return nil
 }

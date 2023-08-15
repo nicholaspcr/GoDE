13a831a4b57d6b3eab9f1adf53f6bc8412faee41
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-08-14 23:43:33 -0300

    Add log Option creator in decli internal utils pkg

---
+++ cmd/decli/internal/utils/log.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"github.com/nicholaspcr/GoDE/internal/log"
+)
+
+// LogOptionsFromConfig returns a slice of log Options after processing the log
+// values in the CLI configuration.
+func LogOptionsFromConfig(cfg *log.Config) []log.Option {
+	opts := make([]log.Option, 0, 10)
+	if cfg == nil {
+		return nil
+	}
+	if cfg.Writer != nil {
+		opts = append(opts, log.WithWriter(cfg.Writer))
+	}
+
+	if cfg.Type != "" {
+		opts = append(opts, log.WithType(cfg.Type))
+	}
+
+	if cfg.Level != 0 {
+		opts = append(opts, log.WithLevel(cfg.Level))
+	}
+
+	if cfg.HandlerOptions != nil {
+		opts = append(opts, log.WithHandlerOptions(cfg.HandlerOptions))
+	}
+
+	if cfg.Pretty != nil {
+		opts = append(opts, log.WithPrettyConfig(cfg.Pretty))
+	}
+
+	return opts
+}

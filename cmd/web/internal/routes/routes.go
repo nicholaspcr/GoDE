79253935aee7e2ec33c02593db6e831963a33d1f
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-09-03 18:48:48 -0300

    Update routes and templates with simple HTMX example

---
+++ cmd/web/internal/routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"embed"
+	"html/template"
+
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed templates
+var templateFS embed.FS
+
+// Route defines a set of routes for the API. It attaches the routes to the gin.RouterGroup provided.
+type Route func(*gin.RouterGroup)
+
+// Routes is a list of predefined routes for the API.
+var Routes = []Route{
+	BaseRoutes,
+}
+
+// BaseRoutes defines the base routes for the API.
+func BaseRoutes(r *gin.RouterGroup) {
+	r.GET("/", func(c *gin.Context) {
+		t := template.Must(template.ParseFS(templateFS, "templates/index.html"))
+		t.Execute(c.Writer, nil)
+	})
+	r.GET("/name", func(c *gin.Context) {
+		t := template.Must(template.ParseFS(templateFS, "templates/name.html"))
+		t.Execute(c.Writer, gin.H{"name": "Gopher"})
+	})
+}

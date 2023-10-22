843953e86af4101324bf0ac888561073e1b38fe0
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-10-22 20:14:23 -0300

    Update web's routes

--- cmd/web/internal/routes/routes.go
+++ cmd/web/internal/routes/routes.go
@@ -2,15 +2,55 @@ package routes
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
+	"io/fs"
 
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed templates
-var templateFS embed.FS
+const templatesDir = "templates"
 
-// Route defines a set of routes for the API. It attaches the routes to the gin.RouterGroup provided.
+var (
+	//go:embed templates/*
+	files     embed.FS
+	templates map[string]*template.Template
+)
+
+func init() {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+	tmplFiles, err := fs.ReadDir(files, templatesDir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tmpl := range tmplFiles {
+		if tmpl.IsDir() {
+			continue
+		}
+
+		pt, err := template.ParseFS(
+			files,
+			templatesDir+"/"+tmpl.Name(),
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		templates[tmpl.Name()] = pt
+	}
+
+	srvPaths := []string{}
+	for k := range templates {
+		srvPaths = append(srvPaths, k)
+	}
+	fmt.Println("Serving templates:", srvPaths)
+}
+
+// Route defines a set of routes for the API. It attaches the routes to the
+// gin.RouterGroup provided.
 type Route func(*gin.RouterGroup)
 
 // Routes is a list of predefined routes for the API.
@@ -21,12 +61,34 @@ var Routes = []Route{
 // baseRoutes defines the base routes for the API.
 func baseRoutes(r *gin.RouterGroup) {
 	r.GET("/", func(c *gin.Context) {
-		t := template.Must(template.ParseFS(templateFS, "templates/index.html"))
-		t.Execute(c.Writer, nil)
+		t := templates["index.html"]
+		if err := t.Execute(c.Writer, nil); err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
 	})
 
-	r.GET("/name", func(c *gin.Context) {
-		t := template.Must(template.ParseFS(templateFS, "templates/name.html"))
-		t.Execute(c.Writer, gin.H{"name": "Gopher"})
+	r.GET("/login", func(c *gin.Context) {
+		t := templates["login.html"]
+		if err := t.Execute(c.Writer, nil); err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+	})
+
+	r.GET("/register", func(c *gin.Context) {
+		t := templates["register.html"]
+		if err := t.Execute(c.Writer, nil); err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+	})
+
+	r.GET("/home", func(c *gin.Context) {
+		t := templates["home.html"]
+		if err := t.Execute(c.Writer, nil); err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
 	})
 }

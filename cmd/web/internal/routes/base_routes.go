fe60af13c0ab445e5f622cddde245fd08efdb8e0
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-10-22 23:13:41 -0300

    Update pages layout

--- cmd/web/internal/routes/base_routes.go
+++ cmd/web/internal/routes/base_routes.go
@@ -20,6 +20,18 @@ func baseRoutes(r *echo.Group) {
 		return nil
 	})
 
+	r.GET("/home", func(c echo.Context) error {
+		t, ok := templates["home.html"]
+		if !ok {
+			return c.String(http.StatusNoContent, "Template not found")
+		}
+		if err := t.Execute(c.Response().Writer, nil); err != nil {
+			c.Response().Status = http.StatusInternalServerError
+		}
+		c.Response().Status = http.StatusOK
+		return nil
+	})
+
 	r.GET("/login", func(c echo.Context) error {
 		t, ok := templates["login.html"]
 		if !ok {
@@ -34,18 +46,6 @@ func baseRoutes(r *echo.Group) {
 
 	r.GET("/register", func(c echo.Context) error {
 		t, ok := templates["register.html"]
-		if !ok {
-			return c.String(http.StatusNoContent, "Template not found")
-		}
-		if err := t.Execute(c.Response().Writer, nil); err != nil {
-			c.Response().Status = http.StatusInternalServerError
-		}
-		c.Response().Status = http.StatusOK
-		return nil
-	})
-
-	r.GET("/home", func(c echo.Context) error {
-		t, ok := templates["home.html"]
 		if !ok {
 			return c.String(http.StatusNoContent, "Template not found")
 		}

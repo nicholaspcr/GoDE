698f2bc9843c1125d7d42742110d74669236861d
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-10-30 21:58:38 -0300

    Add custom navbar items to templates

--- cmd/web/internal/routes/base_routes.go
+++ cmd/web/internal/routes/base_routes.go
@@ -1,10 +1,64 @@
 package routes
 
 import (
+	"bytes"
+	"html/template"
+	"log/slog"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+type (
+	dataMap   map[string]any
+	dataArray []any
+)
+
+func leftNavbar() string {
+	var buff bytes.Buffer
+	t, ok := templates["navbar-set.html"]
+	if !ok {
+		slog.Error("Error fetching template: navbar-set")
+		return ""
+	}
+	err := t.Execute(&buff, dataArray{
+		dataMap{
+			"URL":           "/home",
+			"Name":          "Home",
+			"ShouldPushURL": true,
+			"PushURL":       "/",
+		},
+	})
+	if err != nil {
+		slog.Error("Error parsing template: ", err)
+		return ""
+	}
+	return buff.String()
+}
+
+func rightNavbar() string {
+	var buff bytes.Buffer
+	t, ok := templates["navbar-set.html"]
+	if !ok {
+		slog.Error("Error fetching template: navbar-set")
+		return ""
+	}
+	err := t.Execute(&buff, dataArray{
+		dataMap{
+			"URL":  "/login",
+			"Name": "Login",
+		},
+		dataMap{
+			"URL":  "/register",
+			"Name": "Register",
+		},
+	})
+	if err != nil {
+		slog.Error("Error parsing template: ", err)
+		return ""
+	}
+	return buff.String()
+}
 
 // baseRoutes defines the base routes for the API.
 func baseRoutes(r *echo.Group) {
@@ -13,7 +67,11 @@ func baseRoutes(r *echo.Group) {
 		if !ok {
 			return c.String(http.StatusNoContent, "Template not found")
 		}
-		if err := t.Execute(c.Response().Writer, nil); err != nil {
+		data := dataMap{
+			"LeftNav":  dataArray{template.HTML(leftNavbar())},
+			"RightNav": dataArray{template.HTML(rightNavbar())},
+		}
+		if err := t.Execute(c.Response().Writer, data); err != nil {
 			c.Response().Status = http.StatusInternalServerError
 		}
 		c.Response().Status = http.StatusOK

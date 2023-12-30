f33143e531c08d6b9d0b2ff861dfc1ddbb7a1e05
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-12-30 23:59:03 +0000

    dev: Remove old proto generated golang code

--- pkg/api/api.go
+++
@@ -1,8 +0,0 @@
-// Package api provides the Golang implementation of the API defined in the
-// proto files.
-package api
-
-import "google.golang.org/protobuf/types/known/emptypb"
-
-// Empty is a helper variable to avoid creating a new empty message every time.
-var Empty = &emptypb.Empty{}

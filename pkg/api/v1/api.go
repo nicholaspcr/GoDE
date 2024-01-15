cfa6c6d5440d6806453a996f494797026273144e
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-01-15 16:39:18 -0300

    api: Generate proto golang code

--- pkg/api/v1/api.go
+++ pkg/api/v1/api.go
@@ -1,6 +1,8 @@
+// Package api contains API Schema definitions for the deserver.
+
 package api
 
 import "google.golang.org/protobuf/types/known/emptypb"
 
-// Empty is an empty protobuf message
+// Empty is an empty protobuf message.
 var Empty = &emptypb.Empty{}

1513cae606c3730d034c3b7ef82137f1d04db239
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-01-15 18:33:36 -0300

    Add default tenant to deserver startup

--- cmd/deserver/internal/commands/root.go
+++ cmd/deserver/internal/commands/root.go
@@ -6,6 +6,7 @@ import (
 	"github.com/nicholaspcr/GoDE/cmd/deserver/internal/server"
 	"github.com/nicholaspcr/GoDE/internal/log"
 	"github.com/nicholaspcr/GoDE/internal/store"
+	"github.com/nicholaspcr/GoDE/internal/tenant"
 	"github.com/spf13/cobra"
 )
 
@@ -20,6 +21,7 @@ proto files. Requests can be made via gRPC or HTTP.`,
 		// Set default logger.
 		logger := log.New()
 		slog.SetDefault(logger)
+		ctx = tenant.NewContext(ctx, tenant.DefaultTenant)
 
 		st, err := store.New(ctx)
 		if err != nil {

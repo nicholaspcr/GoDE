485e89e92654b4f694c3b7a8daa89bb6f2b249ef
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-01-15 18:34:12 -0300

    store: Update tenant store

--- internal/store/gorm/tenant.go
+++ internal/store/gorm/tenant.go
@@ -1,8 +1,43 @@
 package gorm
 
-// TODO: Add tenant operations to inject its value on the context.
+import (
+	"context"
+
+	"github.com/nicholaspcr/GoDE/pkg/api/v1"
+	"gorm.io/gorm"
+)
 
 // Tenant is a model for the tenant table.
 type TenantModel struct {
-	Name string `gorm:"primary_key,size:50"`
+	ID string `gorm:"primary_key,size:50"`
+}
+
+type tenantStore struct{ *gorm.DB }
+
+func newTenantStore(db *gorm.DB) *tenantStore { return &tenantStore{db} }
+
+func (st *tenantStore) Create(ctx context.Context, tnt *api.Tenant) error {
+	tenant := TenantModel{ID: tnt.GetIds().TenantId}
+	st.DB.Create(&tenant)
+	return nil
+}
+
+func (st *tenantStore) Get(
+	ctx context.Context, tntIDs *api.TenantIDs,
+) (*api.Tenant, error) {
+	var tnt TenantModel
+	tx := st.First(&tnt, "id = ?", tntIDs.TenantId)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	return &api.Tenant{Ids: &api.TenantIDs{TenantId: tnt.ID}}, nil
+}
+
+func (st *tenantStore) Delete(ctx context.Context, tntIDs *api.TenantIDs) error {
+	var tenant TenantModel
+	tx := st.First(&tenant, "id = ?", tntIDs.TenantId)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
 }

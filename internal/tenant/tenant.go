eeb7f552b43b64dda1fca856aeb9c6d4542e1137
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-01-24 19:56:11 -0300

    dev: Update tenant pkg

--- internal/tenant/tenant.go
+++ internal/tenant/tenant.go
@@ -3,25 +3,22 @@ package tenant
 
 import (
 	"context"
-	"errors"
 
 	"github.com/nicholaspcr/GoDE/pkg/api/v1"
 )
 
-var tenantKey = struct{}{}
-
-var errNotPresent = errors.New("tenant not present in context")
+type tenantKey = struct{}
 
 // FromContext returns the Tenant value stored in ctx, if not present it returns an error.
-func FromContext(ctx context.Context) (*api.Tenant, error) {
-	tenant, ok := ctx.Value(tenantKey).(*api.Tenant)
+func FromContext(ctx context.Context) *api.Tenant {
+	tenant, ok := ctx.Value(tenantKey{}).(*api.Tenant)
 	if !ok {
-		return nil, errNotPresent
+		return DefaultTenant
 	}
-	return tenant, nil
+	return tenant
 }
 
 // NewContext returns a new context with the Tenant value stored in it.
 func NewContext(ctx context.Context, tenant *api.Tenant) context.Context {
-	return context.WithValue(ctx, tenantKey, tenant)
+	return context.WithValue(ctx, tenantKey{}, tenant)
 }

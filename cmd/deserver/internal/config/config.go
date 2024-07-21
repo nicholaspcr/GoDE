a1c756d93be06e1e7b7a1975d94541bb8fec1e63
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-07-20 23:24:49 -0300

    deserver: Make internal config for the executable

--- cmd/deserver/internal/config/config.go
+++ cmd/deserver/internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"encoding/json"
 
+	"gopkg.in/yaml.v3"
+
 	"github.com/nicholaspcr/GoDE/cmd/deserver/internal/server"
 	"github.com/nicholaspcr/GoDE/internal/log"
 	"github.com/nicholaspcr/GoDE/internal/store"
@@ -15,8 +17,18 @@ type DeServer struct {
 	Server server.Config `json:"server" yaml:"server"`
 }
 
+// StringifyJSON returns a string with the JSON object of the configuration.
 func (cfg *DeServer) StringifyJSON() (string, error) {
 	b, err := json.MarshalIndent(cfg, "", "    ")
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// StringifyYAML returns a string block with the yaml configuration contents.
+func (cfg *DeServer) StringifyYAML() (string, error) {
+	b, err := yaml.Marshal(cfg)
 	if err != nil {
 		return "", err
 	}
--- cmd/deserver/internal/config/default.go
+++ cmd/deserver/internal/config/default.go
@@ -1,9 +1,16 @@
 package config
 
-import "github.com/nicholaspcr/GoDE/internal/log"
+import (
+	"github.com/nicholaspcr/GoDE/cmd/deserver/internal/server"
+	"github.com/nicholaspcr/GoDE/internal/log"
+	"github.com/nicholaspcr/GoDE/internal/store"
+)
 
+// Default configuration of the deserver binary.
 func Default() *DeServer {
 	return &DeServer{
-		Log: log.Config,
+		Log:    log.DefaultConfig(),
+		Store:  store.Config{},
+		Server: server.Config{},
 	}
 }

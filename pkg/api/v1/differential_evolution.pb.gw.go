214e75f495a31a466e3fac631d6110de349ceadc
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-07-27 15:35:06 -0300

    api: Update DifferentialEvolution /de/run from GET to a POST

--- pkg/api/v1/differential_evolution.pb.gw.go
+++ pkg/api/v1/differential_evolution.pb.gw.go
@@ -203,7 +203,7 @@ func RegisterDifferentialEvolutionServiceHandlerServer(ctx context.Context, mux 
 
 	})
 
-	mux.Handle("GET", pattern_DifferentialEvolutionService_Run_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_DifferentialEvolutionService_Run_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -335,7 +335,7 @@ func RegisterDifferentialEvolutionServiceHandlerClient(ctx context.Context, mux 
 
 	})
 
-	mux.Handle("GET", pattern_DifferentialEvolutionService_Run_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_DifferentialEvolutionService_Run_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)

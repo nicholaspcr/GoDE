1158dcc982e4ee38da7a871bb3df63830fe906d2
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-08-06 17:47:16 -0300

    decli: Add logic to register command

--- cmd/decli/internal/commands/register.go
+++ cmd/decli/internal/commands/register.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"net/http"
+	"net/url"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,6 +12,17 @@ var registerCmd = &cobra.Command{
 	Use:   "register <username> <password>",
 	Short: "Creates an account",
 	RunE: func(cmd *cobra.Command, _ []string) error {
-		return nil
+		registerAddr := cfg.Server.HTTPAddr + "/register"
+
+		_, err := http.PostForm(registerAddr, url.Values{
+			"name":     []string{"nicholaspcr"},
+			"password": []string{"password"},
+			"email":    []string{"nicholaspcr@gmail.com"},
+		})
+		return err
 	},
+}
+
+func init() {
+	RootCmd.AddCommand(registerCmd)
 }

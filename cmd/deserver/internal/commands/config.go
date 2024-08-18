43053ec8acbf1b5eb1a59253e9996619eced9c93
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-08-18 01:29:40 -0300

    deserver: Update config command

--- cmd/deserver/internal/commands/config.go
+++ cmd/deserver/internal/commands/config.go
@@ -7,21 +7,47 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// ConfigCmd handles commands related to the configuration of the server.
-var ConfigCmd = &cobra.Command{
+var (
+	ofJson bool
+	ofYaml bool
+)
+
+// configCmd handles commands related to the configuration of the server.
+var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Handles configuration for the deserver",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if err := cmd.ValidateFlagGroups(); err != nil {
+			return err
+		}
+		if err := cmd.ValidateRequiredFlags(); err != nil {
+			return err
+		}
+		return nil
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		cfgString, err := cfg.StringifyJSON()
+		var cfgString string
+		var err error
+
+		if ofJson {
+			cfgString, err = cfg.StringifyJSON()
+		}
+		if ofYaml {
+			cfgString, err = cfg.StringifyYAML()
+		}
+
 		if err != nil {
 			return err
 		}
-		slog.Info("Printing the deserver configuration")
+		slog.Debug("Printing the deserver configuration")
 		fmt.Println(cfgString)
 		return nil
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(ConfigCmd)
+	configCmd.Flags().BoolVar(&ofJson, "json", true, "Output in JSON")
+	configCmd.Flags().BoolVar(&ofYaml, "yaml", false, "Output in YAML")
+	configCmd.MarkFlagsMutuallyExclusive("json", "yaml")
+	rootCmd.AddCommand(configCmd)
 }

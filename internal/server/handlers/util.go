51b1c38e4c4f65e6ec324dfdd84d89bc599dadcb
Nicholas Cristofaro <nicholaspcr@gmail.com>
2025-11-09 22:20:23 -0300

    chore: remove unused generatePopulation function
    Removes unused helper function to address linter warning

--- internal/server/handlers/util.go
+++ internal/server/handlers/util.go
@@ -1,12 +1,1 @@
 package handlers
-
-import (
-	"math/rand"
-
-	"github.com/nicholaspcr/GoDE/pkg/models"
-)
-
-// generatePopulation creates a population without objective values calculated
-func generatePopulation(p models.PopulationParams, random *rand.Rand) (models.Population, error) {
-	return models.GeneratePopulation(p, random)
-}

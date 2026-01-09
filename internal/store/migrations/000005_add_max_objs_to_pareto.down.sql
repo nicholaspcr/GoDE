7d4ca1d250d0ebb8c7135ce80c823d619c036c14
Nicholas Cristofaro <nicholaspcr@gmail.com>
2026-01-09 18:29:26 -0300

    Add migration for max_objs_json column in pareto_sets

---
+++ internal/store/migrations/000005_add_max_objs_to_pareto.down.sql
@@ -0,0 +1,2 @@
+-- Remove max_objs_json column from pareto_sets table
+ALTER TABLE pareto_sets DROP COLUMN max_objs_json;
---
+++ internal/store/migrations/000005_add_max_objs_to_pareto.up.sql
@@ -0,0 +1,2 @@
+-- Add max_objs_json column to pareto_sets table
+ALTER TABLE pareto_sets ADD COLUMN max_objs_json TEXT;
